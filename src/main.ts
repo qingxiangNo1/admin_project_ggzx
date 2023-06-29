f3468b25a142cc09132f15e4c524f173a0a1304b
qingxiangNo1 <741140559@qq.com>
2023-06-29 21:15:00 +0800

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus';
+import App from '@/App.vue'
+//引入element-plus插件与样式
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
-// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+//配置element-plus国际化
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+//获取应用实例对象
 const app = createApp(App)
+//element-plus国际化配置
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+//安装elementui-plus插件
 app.use(ElementPlus)
+//svg需要的配置代码
+// import 'virtual:svg-icons-register'
+//使用自定义函数：注册全局组件
+// import globalComponent from '@/components'
+// app.use(globalComponent)
+//引入全局样式
+// import '@/style/index.scss'
+//将应用挂载到挂载点上
 app.mount('#app')
