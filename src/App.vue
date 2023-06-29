4f583595e226ce6c23fe8cdae606776c26d0fd1e
qingxiangNo1 <741140559@qq.com>
2023-06-29 11:39:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>woaini</h1>
+    <el-button type="primary">hhhh</el-button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+div {
+  width: 600px;
+  height: 800px;
+  background: yellowgreen;
+  h1 {
+    color: hotpink;
+  }
+}
+</style>
+
