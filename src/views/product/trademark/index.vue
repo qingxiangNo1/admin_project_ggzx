d2a98e70a43094c025af530aec766a27212e5376
qingxiangNo1 <741140559@qq.com>
2023-07-07 13:55:51 +0800

    55.品牌管理模块数据展示

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -2,11 +2,24 @@
     <div>
         <el-card class="box-card">
             <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
-            <el-table border="true">
-                <el-table-column label="序号" align="center" width="80px"></el-table-column>
-                <el-table-column label="品牌名称"></el-table-column>
-                <el-table-column label="品牌LOGO"></el-table-column>
-                <el-table-column label="品牌操作"></el-table-column>
+            <el-table border="true" :data="trademarkArr">
+                <el-table-column label="序号" align="center" width="80px" type="index"></el-table-column>
+                <el-table-column label="品牌名称" prop="tmName">
+                </el-table-column>
+                <el-table-column label="品牌LOGO">
+                    <template #="{ row, $index }">
+                        <img :src="row.logoUrl" alt="图片不存在" style="width: 100px;height: 100px;">
+                    </template>
+                </el-table-column>
+                <el-table-column label="品牌操作">
+                    <template #='{row,index}'>
+<el-button type="primary" size="small" icon="Edit"></el-button>
+<el-button type="danger" size="small" icon="Delete"></el-button>
+
+
+
+                    </template>
+                </el-table-column>
             </el-table>
             <el-pagination v-model:current-page="pageNo" v-model:page-size="limit" :page-sizes="[3, 4, 5, 6]"
                 background='true' layout=" prev, pager, next, jumper,->,sizes,total" :total="400" />
@@ -16,16 +29,21 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import {reqHasTrademark} from '@/api/product/trademark'
+import { reqHasTrademark } from '@/api/product/trademark'
 import { onMounted } from 'vue';
 onMounted(() => {
     getHasTrademark()
 });
 let pageNo = ref(1);
 let limit = ref(3);
-const  getHasTrademark = async() => {
-   let result = await reqHasTrademark(pageNo.value,limit.value)
-   console.log(result);
+let total = ref(0);
+let trademarkArr = ref([])
+const getHasTrademark = async () => {
+    let result = await reqHasTrademark(pageNo.value, limit.value)
+    if (result.code == 200) {
+        trademarkArr.value = result.data.records;
+        total.value = result.data.total;
+    }
 }
 </script>
 
