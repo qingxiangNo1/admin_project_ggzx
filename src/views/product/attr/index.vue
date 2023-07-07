ef2646fae040cbc7abd5e857d9969cc0d86b4710
qingxiangNo1 <741140559@qq.com>
2023-07-07 20:27:25 +0800

    平台属性管理模块静态的搭建

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-<h1>shu xing guan li</h1>
+        <el-card>
+            <el-form ref="form" label-width="80px" :inline="true" size="normal">
+                <el-form-item label="一级分类" size="normal">
+                    <el-select label="上海">
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="二级分类" size="normal">
+                    <el-select label="上海">
+                    </el-select>
+
+                </el-form-item>
+                <el-form-item label="三级分类" size="normal">
+                    <el-select label="上海">
+                    </el-select>
+
+
+                </el-form-item>
+
+            </el-form>
+
+        </el-card>
+        <el-card>
+            <el-button type="primary" size="default" icon="Plus">添加属性</el-button>
+            <el-table border stripe >
+                <el-table-column label='序号' type="index" width="80px" align="center"/>
+                <el-table-column label="属性名称" width="200px" align="center" />
+                <el-table-column label="属性值名称" align="center"/>
+                <el-table-column label="操作" width="200px" align="center" />
+            </el-table>
+
+        </el-card>
     </div>
 </template>
 
@@ -8,6 +38,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
