7ed204f616aa87f31578dfbe8894b233fb12fce2
qingxiangNo1 <741140559@qq.com>
2023-07-23 19:08:39 +0800

    92.手机新增sku的数据上

--- src/api/product/spu/index.ts
+++ src/api/product/spu/index.ts
@@ -5,7 +5,9 @@ import {
     SpuHasImg,
     SaleAttrResponseData,
     HasSaleAttrResponseData,
-    SpuData
+    SpuData,
+    SkuData,
+    SkuInfoData,
 } from "./type";
 enum API {
     //获取已有的SPU的数据
@@ -22,6 +24,13 @@ enum API {
     ADDSPU_URL = '/admin/product/saveSpuInfo',
     //更新已有的SPU
     UPDATESPU_URL = '/admin/product/updateSpuInfo',
+    //追加一个新增的SKU地址
+    ADDSKU_URL = '/admin/product/saveSkuInfo',
+    //查看某一个已有的SPU下全部售卖的商品
+    SKUINFO_URL = '/admin/product/findBySpuId/',
+    //删除已有的SPU
+    REMOVESPU_URL = '/admin/product/deleteSpu/',
+
 }
 export const reqHasSpu = (page: number, limit: number, category3Id: number | string) => request.get<any, HasResponseDate>(API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`)
 //获取全部的SPU的品牌的数据
@@ -47,3 +56,14 @@ export const reqAddOrUpdateSpu = (data: SpuData) => {
         return request.post<any, any>(API.ADDSPU_URL, data)
     }
 }
+//添加SKU的请求方法
+export const reqAddSku = (data: SkuData) =>
+  request.post<any, any>(API.ADDSKU_URL, data)
+
+//获取SKU数据
+export const reqSkuList = (spuId: number | string) =>
+  request.get<any, SkuInfoData>(API.SKUINFO_URL + spuId)
+
+//删除已有的SPU
+export const reqRemoveSpu = (spuId: number | string) =>
+  request.delete<any, any>(API.REMOVESPU_URL + spuId)
--- src/views/product/spu/skuForm.vue
+++ src/views/product/spu/skuForm.vue
@@ -2,22 +2,24 @@
     <div>
         <el-form label-width="100px">
             <el-form-item label="SKU名称">
-                <el-input placeholder="SKU名称" size="normal"></el-input>
+                <el-input placeholder="SKU名称" size="normal" v-model="skuParams.skuName"></el-input>
             </el-form-item>
             <el-form-item label="价格(元)">
-                <el-input placeholder="价格(元)" type="number"></el-input>
+                <el-input placeholder="价格(元)" type="number" v-model="skuParams.price"></el-input>
             </el-form-item>
             <el-form-item label="重量(克)">
-                <el-input placeholder="重量(克)" type="number"></el-input>
+                <el-input placeholder="重量(克)" type="number" v-model="skuParams.weight"></el-input>
             </el-form-item>
             <el-form-item label="SKU描述">
-                <el-input placeholder="SKU描述" type="textarea"></el-input>
+                <el-input placeholder="SKU描述" type="textarea" v-model="skuParams.skuDesc"></el-input>
             </el-form-item>
             <el-form-item label="平台属性">
                 <el-form :inline="true" label-width="120px">
-                    <el-form-item  v-for="(attrArrValue, index) in attrArr" :key="index" :label="attrArrValue.attrName" style="margin: 10px 10px;">
+                    <el-form-item v-for="(attrArrValue, index) in attrArr" :key="index" :label="attrArrValue.attrName"
+                        style="margin: 10px 10px;">
                         <el-select placeholder="请选择">
-                            <el-option  v-for="(item, index) in attrArrValue.attrValueList" :key="index" :label="item.valueName"></el-option>
+                            <el-option v-for="(item, index) in attrArrValue.attrValueList" :key="index"
+                                :label="item.valueName"></el-option>
                         </el-select>
                     </el-form-item>
                 </el-form>
@@ -26,7 +28,8 @@
                 <el-form :inline="true">
                     <el-form-item v-for="(saleArrValue, index) in saleArr" :key="index" :label="saleArrValue.saleAttrName">
                         <el-select placeholder="请选择">
-                            <el-option v-for="(item, index) in saleArrValue.spuSaleAttrValueList" :key="index" :label="item.saleAttrValueName"></el-option>
+                            <el-option v-for="(item, index) in saleArrValue.spuSaleAttrValueList" :key="index"
+                                :label="item.saleAttrValueName"></el-option>
                         </el-select>
                     </el-form-item>
                 </el-form>
@@ -35,14 +38,14 @@
                 <el-table border style="margin: 10px 0px;" :data="imgArr">
                     <el-table-column type="selection" align="center" width="80px"></el-table-column>
                     <el-table-column label="图片" align="center">
-                        <template #='{row,$index}'>
-                          <img :src="row.imgUrl" alt="" style="width: 100px; height: 100px;">
+                        <template #='{ row, $index }'>
+                            <img :src="row.imgUrl" alt="" style="width: 100px; height: 100px;">
                         </template>
                     </el-table-column>
                     <el-table-column label="名称" align="center" prop="imgName"></el-table-column>
                     <el-table-column label="操作" align="center">
-                        <template #='{row,$index}'>
-                        <el-button type="primary" size="small" @click="">设为默认</el-button>
+                        <template #='{ row, $index }'>
+                            <el-button type="primary" size="small" @click="">设为默认</el-button>
                         </template>
                     </el-table-column>
                 </el-table>
@@ -54,27 +57,49 @@
 </template>
 
 <script setup lang="ts">
-import {reqAttr} from '@/api/product/attr'
-import {reqSpuImageList,reqSpuHasSaleAttr} from '@/api/product/spu'
+import { reqAttr } from '@/api/product/attr'
+import { SkuData } from '@/api/product/spu/type'
+import { reqSpuImageList, reqSpuHasSaleAttr } from '@/api/product/spu'
 import { ref } from 'vue'
 let $emit = defineEmits(['changeScene'])
 let attrArr = ref<any>()
 let imgArr = ref<any>()
 let saleArr = ref<any>()
+//收集SKU的参数
+let skuParams = ref<SkuData>({
+    //父组件传递过来的数据
+    "category3Id": "",//三级分类的ID
+    "spuId": "",//已有的SPU的ID
+    "tmId": "",//SPU品牌的ID
+    //v-model收集
+    "skuName": "",//sku名字
+    "price": "",//sku价格
+    "weight": "",//sku重量
+    "skuDesc": "",//sku的描述
+
+    "skuAttrValueList": [//平台属性的收集
+    ],
+    "skuSaleAttrValueList": [//销售属性
+    ],
+    "skuDefaultImg": "",//sku图片地址
+})
 //取消按钮的回调
 const cancel = () => {
-      $emit('changeScene',{flag:0,params:''})
+    $emit('changeScene', { flag: 0, params: '' })
 }
 //添加sku的自定义函数
-const initAddSku = async(c1Id:number,c2Id:number,spu:any) => {
-     let result:any = await reqAttr(c1Id,c2Id,spu.category3Id)
-     let result1:any = await reqSpuImageList(spu.id)
-     let result2:any = await reqSpuHasSaleAttr(spu.id)
-     imgArr.value = result1.data
-     attrArr.value = result.data
-     saleArr.value = result2.data
+const initAddSku = async (c1Id: number, c2Id: number, spu: any) => {
+    skuParams.value.category3Id = spu.category3Id
+    skuParams.value.spuId = spu.id
+    skuParams.value.tmId = spu.tmId
+    let result: any = await reqAttr(c1Id, c2Id, spu.category3Id)
+    let result1: any = await reqSpuImageList(spu.id)
+    let result2: any = await reqSpuHasSaleAttr(spu.id)
+    imgArr.value = result1.data
+    attrArr.value = result.data
+    saleArr.value = result2.data
 }
-defineExpose({initAddSku})
+defineExpose({ initAddSku })
 </script>
 
 <style scoped></style>
