c6c6957d1e8bd66a9807dd186489540920416a72
qingxiangNo1 <741140559@qq.com>
2023-07-24 17:10:41 +0800

    97.展示已有sku

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,36 +1,57 @@
 <template>
     <div>
-     <el-card>
-        <el-table border style="margin: 10px 0px;"> 
-            <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
-            <el-table-column label="名称" width="150px" align="center" ></el-table-column>
-            <el-table-column label="描述" width="150px" align="center" show-overflow-tooltip></el-table-column>
-            <el-table-column label="图片" width="150px" align="center" ></el-table-column>
-            <el-table-column label="重量" width="150px" align="center" ></el-table-column>
-            <el-table-column label="价格" width="150px" align="center" ></el-table-column>
-            <el-table-column label="操作" width="250px" align="center" fixed="right"></el-table-column>       
-        </el-table>
-        <el-pagination
-      v-model:current-page="pageNo"
-      v-model:page-size="pageSize"
-      :page-sizes="[3, 5, 7, 9]"
-      :background="true"
-      layout="prev, pager, next, jumper,->,total, sizes"
-      :total="total"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    />
-     </el-card>
+        <el-card>
+            <el-table border style="margin: 10px 0px;" :data="skuArr">
+                <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
+                <el-table-column label="名称" width="150px" align="center" prop="skuName"></el-table-column>
+                <el-table-column label="描述" width="300px" align="center" prop="skuDesc"
+                    show-overflow-tooltip></el-table-column>
+                <el-table-column label="图片" width="150px" align="center">
+                    <template #='{ row, $index }'>
+                        <img :src="row.skuDefaultImg" alt="" style="width: 100px;height: 100px;">
+                    </template>
+                </el-table-column>
+                <el-table-column label="重量" width="150px" align="center" prop="weight"></el-table-column>
+                <el-table-column label="价格" width="150px" align="center" prop="price"></el-table-column>
+                <el-table-column label="操作" width="300px" align="center" fixed="right">
+                    <template #='{ row, index }'>
+                        <el-button type="primary" size="small" @click="" icon="Top"></el-button>
+                        <el-button type="primary" size="small" @click="" icon="Edit"></el-button>
+                        <el-button type="primary" size="small" @click="" icon="InfoFilled"></el-button>
+                        <el-button type="primary" size="small" @click="" icon="Delete"></el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <el-pagination v-model:current-page="pageNo" v-model:page-size="pageSize" :page-sizes="[3, 5, 7, 9]"
+                :background="true" layout="prev, pager, next, jumper,->,total, sizes" :total="total"
+                @size-change="handleCurrentChange" @current-change="getHasSku" />
+        </el-card>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { SkuData, SkuResponseData } from '@/api/product/sku/type'
+import { reqSkuList } from '@/api/product/sku'
 let pageNo = ref<number>(1)
 let pageSize = ref<number>(3)
-let total = ref<number>(30)
+let total = ref<number>(0)
+let skuArr = ref<SkuData[]>([])
+onMounted(() => {
+    getHasSku()
+})
+const getHasSku = async (pager = 1) => {
+    pageNo.value = pager
+    let result: SkuResponseData = await reqSkuList(pageNo.value, pageSize.value)
+    if (result.code == 200) {
+        skuArr.value = result.data.records
+        total.value = result.data.total
+    }
+}
+const handleCurrentChange = (num: number) => {
+    getHasSku()
+}
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
