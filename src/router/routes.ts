8467b6aaddd734b57efdf69c8c6c3d489e1d591a
qingxiangNo1 <741140559@qq.com>
2023-07-29 17:12:40 +0800

    140.菜单权限的实现

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,99 +1,90 @@
+//对外暴露配置路由(常量路由):全部用户都可以访问到的路由
 export const constantRoute = [
     {
-        //登录
-        path: '/login',
-        component: () => import('@/views/login/index.vue'),
-        name: 'login',
-        meta: {
-            title: '登录',//菜单标题
-            hidden: true,//代表路由标题在菜单中是否隐藏 true：隐藏 false：不隐藏
-            icon: 'Link',
-        }
+      //登录
+      path: '/login',
+      component: () => import('@/views/login/index.vue'),
+      name: 'login',
+      meta: {
+        title: '登录', //菜单标题
+        hidden: true, //代表路由标题在菜单中是否隐藏  true:隐藏 false:不隐藏
+        icon: 'Promotion', //菜单文字左侧的图标,支持element-plus全部图标
+      },
     },
     {
-        //登陆成功后展示数据的路由
-        path: '/',
-        component: () => import('@/layout/index.vue'),
-        name: 'layout',
-        meta: {
-            hidden: false,//代表路由标题在菜单中是否隐藏 true：隐藏 false：不隐藏
-            icon: 'Link',
-        },
-        redirect: '/home',
-        children: [
-            {
-                path: '/home',
-                component: () => import('@/views/home/index.vue'),
-                name: 'home',
-                meta: {
-                    title: '首页',
-                    hidden: false,//代表路由标题在菜单中是否隐藏 true：隐藏 false：不隐藏
-                    icon: 'HomeFilled',
-
-                }
-            },
-        ]
-    },
-    {
-        //404
-        path: '/404',
-        component: () => import('@/views/404/index.vue'),
-        name: '404',
-        meta: {
-            title: '404',
-            hidden: true,//代表路由标题在菜单中是否隐藏 true：隐藏 false：不隐藏
-            icon: 'Link',
-        }
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        redirect: '/404',
-        name: 'Any',
-        meta: {
-            title: '任意路由',
-            hidden: true,//代表路由标题在菜单中是否隐藏 true：隐藏 false：不隐藏
-            icon: 'Link',
-        }
-    },
-    {
-        path: '/screen',
-        component: () => import('@/views/screen/index.vue'),
-        name: 'Screen',
-        meta: {
+      //登录成功以后展示数据的路由
+      path: '/',
+      component: () => import('@/layout/index.vue'),
+      name: 'layout',
+      meta: {
+        title: '',
+        hidden: false,
+        icon: '',
+      },
+      redirect: '/home',
+      children: [
+        {
+          path: '/home',
+          component: () => import('@/views/home/index.vue'),
+          meta: {
+            title: '首页',
             hidden: false,
-            title: '数据大屏',
-            icon: 'Monitor',
-        }
+            icon: 'HomeFilled',
+          },
+        },
+      ],
     },
+    {
+      //404
+      path: '/404',
+      component: () => import('@/views/404/index.vue'),
+      name: '404',
+      meta: {
+        title: '404',
+        hidden: true,
+        icon: 'DocumentDelete',
+      },
+    },
+    {
+      path: '/screen',
+      component: () => import('@/views/screen/index.vue'),
+      name: 'Screen',
+      meta: {
+        hidden: false,
+        title: '数据大屏',
+        icon: 'Platform',
+      },
+    },
+  ]
+  
+//异步路由
+export const asnycRoute = [
     {
         path: '/acl',
         component: () => import('@/layout/index.vue'),
         name: 'Acl',
         meta: {
             title: '权限管理',
-            hidden: false,
             icon: 'Lock',
         },
-        redirect:'/acl/role',
+        redirect: '/acl/user',
         children: [
-            {
-                path: '/acl/role',
-                component: () => import('@/views/acl/role/index.vue'),
-                name: 'Role',
-                meta: {
-                    title: '角色管理',
-                    hidden: false,
-                    icon: 'Avatar',
-                },
-            },
             {
                 path: '/acl/user',
                 component: () => import('@/views/acl/user/index.vue'),
                 name: 'User',
                 meta: {
                     title: '用户管理',
-                    hidden: false,
                     icon: 'User',
+                },
+            },
+            {
+                path: '/acl/role',
+                component: () => import('@/views/acl/role/index.vue'),
+                name: 'Role',
+                meta: {
+                    title: '角色管理',
+                    icon: 'UserFilled',
                 },
             },
             {
@@ -102,11 +93,10 @@ export const constantRoute = [
                 name: 'Permission',
                 meta: {
                     title: '菜单管理',
-                    hidden: false,
-                    icon: 'Grid',
+                    icon: 'Monitor',
                 },
             },
-        ]
+        ],
     },
     {
         path: '/product',
@@ -114,10 +104,9 @@ export const constantRoute = [
         name: 'Product',
         meta: {
             title: '商品管理',
-            icon: 'Shop',
-            hidden: false,
+            icon: 'Goods',
         },
-        redirect:'/product/trademark',
+        redirect: '/product/trademark',
         children: [
             {
                 path: '/product/trademark',
@@ -125,8 +114,7 @@ export const constantRoute = [
                 name: 'Trademark',
                 meta: {
                     title: '品牌管理',
-                    icon: 'GoodsFilled',
-                    hidden: false,
+                    icon: 'ShoppingCartFull',
                 },
             },
             {
@@ -135,8 +123,7 @@ export const constantRoute = [
                 name: 'Attr',
                 meta: {
                     title: '属性管理',
-                    icon: 'Film',
-                    hidden: false,
+                    icon: 'ChromeFilled',
                 },
             },
             {
@@ -144,9 +131,8 @@ export const constantRoute = [
                 component: () => import('@/views/product/spu/index.vue'),
                 name: 'Spu',
                 meta: {
-                    title: 'Spu管理',
-                    icon: 'ShoppingCart',
-                    hidden: false,
+                    title: 'SPU管理',
+                    icon: 'Calendar',
                 },
             },
             {
@@ -154,11 +140,23 @@ export const constantRoute = [
                 component: () => import('@/views/product/sku/index.vue'),
                 name: 'Sku',
                 meta: {
-                    title: 'Sku管理',
-                    icon: 'ShoppingBag',
-                    hidden: false,
+                    title: 'SKU管理',
+                    icon: 'Orange',
                 },
             },
-        ]
+        ],
     },
 ]
+
+//任意路由
+export const anyRoute = {
+    //任意路由
+    path: '/:pathMatch(.*)*',
+    redirect: '/404',
+    name: 'Any',
+    meta: {
+        title: '任意路由',
+        hidden: true,
+        icon: 'DataLine',
+    },
+}
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,14 +1,31 @@
 //创建用户相关的小仓库
 import { defineStore } from 'pinia'
 //引入登录需要的参数类型
-import { loginFormData, loginResponseData,userInfoReponseData } from '@/api/user/type'
+import { loginFormData, loginResponseData, userInfoReponseData } from '@/api/user/type'
 //引入登录接口
 import { reqLogin, reqLogout, reqUserInfo } from '@/api/user'
 import { userState } from './types/type'
 //引入存储函数
 import { SET_TOKEN, GET_TOKEN, REMOVE_TOKEN } from '@/utils/token'
 //引入路由（常量路由）
-import { constantRoute } from '@/router/routes'
+import { constantRoute, asnycRoute, anyRoute } from '@/router/routes'
+//引入深拷贝方法
+//@ts-expect-error
+import cloneDeep from 'lodash/cloneDeep'
+import router from '@/router'
+//用于过滤当前用户需要展示的异步路由
+function filterAsyncRoute(asnycRoute: any, routes: any) {
+    return asnycRoute.filter((item: any) => {
+        if (routes.includes(item.name)) {
+            if (item.children && item.children.length > 0) {
+                //硅谷333账号:product\trademark\attr\sku
+                item.children = filterAsyncRoute(item.children, routes)
+            }
+            return true
+        }
+    })
+}
+
 //创建用户小仓库
 let useUserStore = defineStore('User', {
     //小仓库存储数据的地方
@@ -26,7 +43,7 @@ let useUserStore = defineStore('User', {
         async userLogin(data: loginFormData) {
             // 登录请求：成功200-》token
             //登录请求：失败201-》错误信息
-            const result:loginResponseData = await reqLogin(data);
+            const result: loginResponseData = await reqLogin(data);
             if (result.code == 200) {
                 // pinia仓库存储一下token
                 this.token = (result.data as string);
@@ -40,10 +57,21 @@ let useUserStore = defineStore('User', {
         },
         // 获取用户信息的方法
         async userInfo() {
-            let result:userInfoReponseData = await reqUserInfo();
+            let result: userInfoReponseData = await reqUserInfo();
             if (result.code == 200) {
                 this.username = result.data.name;
                 this.avatar = result.data.avatar
+                //计算当前用户需要展示的异步路由
+                const userAsyncRoute = filterAsyncRoute(
+                    cloneDeep(asnycRoute),
+                    result.data.routes,
+                )
+                //菜单需要的数据整理完毕
+                this.menuRoutes = [...constantRoute, ...userAsyncRoute, anyRoute]
+                    //目前路由器管理的只有常量路由:用户计算完毕异步路由、任意路由动态追加
+                    ;[...userAsyncRoute, anyRoute].forEach((route: any) => {
+                        router.addRoute(route)
+                    })
                 return 'ok'
             } else {
                 return Promise.reject(new Error(result.message))
@@ -51,7 +79,7 @@ let useUserStore = defineStore('User', {
         },
         // 用户退出登录方法
         async userLogout() {
-            let result:any = await reqLogout();
+            let result: any = await reqLogout();
             if (result.code == 200) {
                 //需要调用一个退出登录接口 ， 使用以下代替
                 this.token = ''
@@ -60,7 +88,7 @@ let useUserStore = defineStore('User', {
                 REMOVE_TOKEN();//移除本地token存储
                 return 'ok'; //退出成功返回一个成功的promise
             } else {
-              return Promise.reject(new Error(result.message))//退出失败返回一个失败的promise
+                return Promise.reject(new Error(result.message))//退出失败返回一个失败的promise
             }
         },
     },
